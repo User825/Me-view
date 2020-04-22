b570861b212ad7f59e775739b5c2a908c7c853fd
user825 <ivan.chazov@gmail.com>
2020-04-22 22:02:24 +0300

    Updates

---
+++ precache-manifest.870a4849c79bc3e448a96b477ece4a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df4611613fc9532e42045f65e8257e0",
+    "url": "/Me-view/index.html"
+  },
+  {
+    "revision": "b0cf86644421ad364bb1",
+    "url": "/Me-view/static/css/2.ba989b92.chunk.css"
+  },
+  {
+    "revision": "22513b73847fae9ffd33",
+    "url": "/Me-view/static/css/main.b8538a77.chunk.css"
+  },
+  {
+    "revision": "b0cf86644421ad364bb1",
+    "url": "/Me-view/static/js/2.796eb15d.chunk.js"
+  },
+  {
+    "revision": "02463abd2ece3f3eea10b0512c401491",
+    "url": "/Me-view/static/js/2.796eb15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22513b73847fae9ffd33",
+    "url": "/Me-view/static/js/main.a3e25360.chunk.js"
+  },
+  {
+    "revision": "118e77babedef186eed6",
+    "url": "/Me-view/static/js/runtime-main.7f9d30e5.js"
+  }
+]);
